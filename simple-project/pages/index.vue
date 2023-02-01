63a3883ac0bcc83373876594f167e5ec8429aff4
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-02-01 23:25:37 +0600

    Nuxtlink

--- simple-project/pages/index.vue
+++ simple-project/pages/index.vue
@@ -1,6 +1,13 @@
  <template>
     <div> 
-        <h1>Home page</h1>
+        <header>
+            <nav>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/products">Store</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
     </div>
  </template>
 
