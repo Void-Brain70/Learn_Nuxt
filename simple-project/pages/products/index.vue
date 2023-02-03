1709492b61fbfb8b3fd67d8e28e5005303294128
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-02-03 14:49:53 +0600

    Product details page

--- simple-project/pages/products/index.vue
+++ simple-project/pages/products/index.vue
@@ -1,16 +1,18 @@
 <template>
-    <div> 
-        <h1>product page</h1>
-        <div v-for="item in products" :key="item">
-           <NuxtLink :to="`/products/${item.id}`" >{{ item.title }}</NuxtLink>
-        </div>
+  <div>
+    <h1 class="text-center font-bold m-2">Product</h1>
+    <hr>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="item in products" :key="item">
+        <!-- <NuxtLink :to="`/products/${item.id}`" >{{ item.title }}</NuxtLink> -->
+        <ProductCard :product="item" />
+      </div>
     </div>
- </template>
+  </div>
+</template>
 
- <script setup>
- const {data : products } = await useFetch('https://fakestoreapi.com/products') 
- </script>
+<script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
 
- <style scoped> 
- 
- </style>
+<style scoped></style>
