fdec9b47179b968a14fb0f077fa970d9d7fd242c
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-02-05 17:06:11 +0600

    server route

---
+++ simple-project/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>About Page</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit commodi nisi quis voluptatum ullam fuga mollitia nulla architecto aliquam animi sit eum temporibus minima, iste iure suscipit nihil corrupti repudiandae.</p>
+      <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+ const { data } = await useFetch('/api/api.js')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
