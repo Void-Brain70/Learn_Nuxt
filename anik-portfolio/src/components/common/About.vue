8b27f3d7dcbd14a2bd083edbf568b05bfe4d1594
Void-Brain70 <dasssanik124102@gmail.com>
2023-06-10 17:12:34 +0600

    about me

---
+++ anik-portfolio/src/components/common/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-[#161616]">
+        <div class="container mx-auto h-screen">
+            <div class="grid grid-cols-2">
+                <div class="flex items-center py-10  px-8 ">
+                    <div class="rounded-3xl bg-blue-500 shadow-2xl shadow-blue-500/50">
+                        <img class="rounded-3xl" src="../../public/me/aboutMe.jpg" />
+                    </div>
+                </div>
+                <div class="flex-col items-center py-10 px-8 ">
+                    <p class=" text-[32px] text-white">My Intro</p>
+                    <p class="text-[42px]  text-white">About Me</p>
+                    <p class="text-[24px] text-white">I am web developer, I create and work with different kind of website or project such as Portfolio
+                        website, LSM etc. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template> 
--- anik-portfolio/src/pages/index.vue
+++ anik-portfolio/src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <CommonHero/>
+        <CommonAbout/>
     </div>
 </template>
 
