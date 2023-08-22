5d998abdeb37d3b5c0eed144932b12c72839e3eb
Anik Chandra <dasssanik124102@gmail.com>
2023-08-22 14:57:50 +0600

    today work

--- anik-portfolio/src/pages/blog.vue
+++ anik-portfolio/src/pages/blog.vue
@@ -1,7 +1,23 @@
 <template>
-    <div class="bg-[#161616] pt-[50px]">
-        <div class="container mx-auto my-auto">
-          <CommonPageHeading textOne="My" textTwo="Blogs" />
-        </div>
-    </div>
+   <section class="body">
+     <div class="pt-[50px]">
+       <div class="container mx-auto my-auto">
+         <CommonPageHeading textOne="My" textTwo="Blogs" />
+       </div>
+       <div>
+         <blog-card/>
+       </div>
+     </div>
+   </section>
 </template>
+<script setup>
+import BlogCard from "~/components/card/BlogCard.vue";
+</script>
+
+<style scoped>
+.body{
+  margin: 0;
+  padding: 0;
+  background-color: #2d3436;
+}
+</style>
