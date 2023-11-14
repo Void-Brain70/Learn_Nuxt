7c88598150c05bf8dd2b4b178bb8266f5deee8d4
Anik Chandra <dasssanik124102@gmail.com>
2023-11-14 23:22:35 +0600

    dasdffsd

--- photography-blog/nuxt.config.ts
+++ photography-blog/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    srcDir: "./src/",
+    css: ['primevue/resources/themes/lara-light-blue/theme.css'],
+    modules: [
+        'nuxt-primevue',
+    ],
+    primevue: {
+        cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities',
+    }
 })
---
+++ photography-blog/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Button label="Submit" class="bg-red-500" />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
